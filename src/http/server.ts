c58e7a9720ac0dea5634351c7e4c0830c50508d8
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2025-01-24 09:11:27 -0300

    initial commit and docker setup for postgresql

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,6 +1,8 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia").listen(3000, () => {
+  console.log("HTTP server running!");
+});
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
