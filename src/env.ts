7c30242395219b0ccdb79a4744e417e4fff5b7b4
albqvictor1508 <albq.victor@gmail.com>
2025-01-24 18:59:08 -0300

    feat: add jwt and cookie to the project

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,8 @@
-import { cleanEnv, url } from "envalid";
+import { cleanEnv, str, url } from "envalid";
 
 export const env = cleanEnv(process.env, {
 	DATABASE_URL: url(),
 	API_BASE_URL: url(),
 	AUTH_REDIRECT_URL: url(),
+	JWT_SECRET_KEY: str(),
 });
